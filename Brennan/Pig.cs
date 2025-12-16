099ebb7329d51cb64973e1db59319f63cefa9f85
Chris.Brennan <brennanc@hotmail.com>
2025-12-16 05:45:03 -0500

    Date: 12/16/2025 05:45:03

--- Brennan/Pig.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Pig : Animal  // Derived class (child) 
-{
-    public override void animalSound()
-    {
-        Console.WriteLine("The pig says: wee wee");
-    }
-}
